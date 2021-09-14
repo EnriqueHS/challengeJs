facd185d41ea7924ab4a6174b023fd41c694549e
Enrique Scapoli <enriquescapoli@gmail.com>
2021-09-14 00:03:00 -0300

    Primer challenge...

---
+++ desafioClase1/js/main.js
@@ -0,0 +1,14 @@
+alert("Primer Desafio");
+
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+let edad = prompt("Su edad");
+
+console.log(nombre + " " + apellido + " " + edad );
+
+alert("OK seguimos");
+
+
+
+
+

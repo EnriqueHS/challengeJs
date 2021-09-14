407ac6178d9be117b4745775420e47053d589f9e
Enrique Scapoli <enriquescapoli@gmail.com>
2021-09-14 10:43:54 -0300

    prompt alert

--- desafioClase1/js/main.js
+++ desafioClase1/js/main.js
@@ -1,4 +1,14 @@
+
+
 alert("Primer Desafio");
+
+let nacionalidad = "Argentino";
+let residencia = "Caba";
+let geocalizacion = nacionalidad + " " + residencia;
+
+console.log(geocalizacion);
+
+alert("Ahora promp");
 
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
@@ -7,8 +17,3 @@ let edad = prompt("Su edad");
 console.log(nombre + " " + apellido + " " + edad );
 
 alert("OK seguimos");
-
-
-
-
-
